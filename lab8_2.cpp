13973c8bb33610a1b3b13b697ff48a639d87b0f3
TenAplus <pongthitheethawat@gmail.com>
2024-12-27 19:33:50 +0700

    Update lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,8 +3,22 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float x){
+	if( x > 90)
+	return 'A';
+	else 
+		if(x > 75)
+		return 'B';
+		else
+			if(x > 60)
+			return 'C';
+			else
+				if(x > 45)
+				return 'D';
+				else
+					return 'F';
 
+}
 int main(){
 	//Input the number of students
 	int N,i = 0;
